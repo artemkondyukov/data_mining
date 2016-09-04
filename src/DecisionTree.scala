53c3747b1aa23dda30629f8a11012ef76fb1d93a
Artem Kondyukov <artemkondyukov@gmail.com>
2016-09-04 11:15:03 +0300

    Implemented tree building using Gini index

--- src/DecisionTree.scala
+++ src/DecisionTree.scala
@@ -1,7 +1,14 @@
+import DecisionTree.Objective.Objective
+
 /**
   * Created by fonturacetamum on 03/09/16.
   */
 object DecisionTree extends App {
+  object Objective extends Enumeration {
+    type Objective = Value
+    val Gini, InformationGain = Value
+  }
+
   val data = List(
     (List("small", "blonde", "brown"), "no"),
     (List("tall",  "dark",   "brown"), "no"),
@@ -31,16 +38,16 @@ object DecisionTree extends App {
 
   // By now is returns Map[A, Any] where Any represents another map
   // TODO reimplement it using Tree class
-  def buildTree[A](data: List[(List[A], A)]): Map[Option[A], Any] = {
+  def buildTree[A](data: List[(List[A], A)], objective: Objective): Map[Set[A], Any] = {
     if (data.isEmpty) throw new java.lang.IllegalArgumentException
 
-    // How many different outcomes are and what's are their frequences
+    // How many different outcomes are and what's are their frequencies
     val outcome_counts = value_counts(data.map(_._2))
     if (outcome_counts.size == 1)
-      return Map((None, outcome_counts.toList.head._1))
+      return Map((Set.empty[A], outcome_counts.toList.head._1))
 
     // There are no features to build tree on
-    if (data.head._1.isEmpty) return Map((None, outcome_counts.toList.head._1))
+    if (data.head._1.isEmpty) return Map((Set.empty[A], outcome_counts.toList.head._1))
 
     // Shortcuts for probability and log2 functions
     def p(valAmount: Int) = valAmount.toDouble / data.length
@@ -56,14 +63,14 @@ object DecisionTree extends App {
       )
 
     def sqr(x: Double): Double = { x * x }
-    val gini_indices = DecisionTree.data.head
+    val gini_indices: List[Set[((Set[A], Set[A]), Double)]] = data.head
       ._1
       .zipWithIndex
       .map(feature =>
         cPowerSet(data.map(row => row._1(feature._2)).toSet).map(comb => {
           val mult = data.count(row => comb._1.contains(row._1(feature._2))).toDouble / data.size
           val multList = List(mult, 1 - mult)
-          List(comb._1, comb._2)
+          comb -> List(comb._1, comb._2)
             .map(combPart => {
               val matching = data.filter(row => combPart.contains(row._1(feature._2)))
               value_counts(matching
@@ -77,52 +84,92 @@ object DecisionTree extends App {
         })
       )
 
+    val maxGiniTuple = gini_indices.zipWithIndex
+      .map(set => set._1.toList.maxBy { _._2 } -> set._2)
+      .max(Ordering[Double].on[((_, Double), _)](_._1._2))
+    val giniSplit = maxGiniTuple._1._1 -> maxGiniTuple._2
 
-    val index = information_gains.indexOf(information_gains.max)
-    def dropIndex(list: List[A]) = list.take(index - 1) ++ list.takeRight(list.length - index - 1)
+    def dropIndex(index: Int)(list: List[A]) = list.take(index - 1) ++ list.takeRight(list.length - index - 1)
+    objective match {
+      case Objective.InformationGain =>
+        val index = information_gains.indexOf(information_gains.max)
 
-    val feature_subtrees = data
-      .map(row => row._1(index))
-      .distinct
-      .map(
-        value => Some(value) -> buildTree(
-          data
-            .filter(row => row._1(index) == value)
-            .map(row => dropIndex(row._1) -> row._2)
-        )
-      )
-    feature_subtrees.toMap
+        val feature_subtrees = data
+          .map(row => row._1(index))
+          .distinct
+          .map(
+            value => Set(value) -> buildTree(
+              data
+                .filter(row => row._1(index) == value)
+                .map(row => dropIndex(index)(row._1) -> row._2),
+              objective
+            )
+          )
+        feature_subtrees.toMap
+      case Objective.Gini => {
+        val feature_subtrees = List(giniSplit._1._1, giniSplit._1._2)
+          .map(
+            value => value -> buildTree(
+             data
+                .filter(row => value.contains(row._1(giniSplit._2)))
+                .map(row => value.size match {
+                  case 1 => dropIndex(giniSplit._2)(row._1) -> row._2
+                  case len => row
+                }),
+              objective
+            )
+          )
+        feature_subtrees.toMap
+      }
+    }
+
   }
 
   // TODO rewrite in functional fashion
-  def printTree(tree: Map[Option[String], Any], tabs: Int = 0): Unit = {
+  def printTree(tree: Map[Set[String], Any], tabs: Int = 0): Unit = {
     tree.toList.foreach(value => {
-      value._1.foreach(node => println("\t" * tabs + node))
+//      value._1.foreach(node => println("\t" * tabs + node))
+      value._1.size match {
+        case 0 =>
+        case len =>  println ("\t" * tabs + value._1.mkString (", ") )
+      }
       value._2 match {
         case s: String => println("\t" * tabs + s)
-        case map: Map[Option[String], Any] => printTree(map, tabs+1)
+        case map: Map[Set[String], Any] => printTree(map, tabs+1)
       }
     })
   }
 
-  def getRules(tree: Map[Option[String], Any]): List[(List[String], Any)] = {
+  def getRules(tree: Map[Set[String], Any]): List[(List[List[String]], Any)] = {
     tree.map(value => value._2 match {
       case string: String => (Nil -> value._2) :: Nil
-      case subtree: Map[Option[String], Map[Option[String], Any]] =>
-        getRules(subtree).map(rule => (value._1.getOrElse("") :: rule._1) -> rule._2)
+      case subtree: Map[Set[String], Map[Set[String], Any]] =>
+        getRules(subtree).map(rule => (value._1.toList :: rule._1) -> rule._2)
     }
     ).toList.flatten
   }
 
-  def getRuleAccuracy(tree: Map[Option[String], Any], rule: List[String]): Double = {
+  def getRuleAccuracy(tree: Map[Set[String], Any], rule: List[List[String]]): Double = {
     val satisfying = rule
       .zipWithIndex
       .foldLeft(data)((intermediate, rulePart) =>
-        intermediate.filter(row => row._1(rulePart._2) == rulePart._1))
+        intermediate.filter(row => rulePart._1.contains(row._1(rulePart._2))))
     value_counts(satisfying.map(_._2)).values
       .max
       .toDouble / satisfying.length
   }
 
-  println(cPowerSet(Set(1, 2, 3)))
+  val decisionTreeInfoGain = buildTree(data, Objective.InformationGain)
+
+  println("Tree: ")
+  printTree(decisionTreeInfoGain)
+  println("\nRules: ")
+  val rules = getRules(decisionTreeInfoGain)
+//  val acc = getRuleAccuracy(decisionTree, List("small", "blonde", "brown"))
+  rules.foreach(rule => print(rule + ". Accuracy: " + getRuleAccuracy(decisionTreeInfoGain, rule._1) + "\n"))
+
+  val decisionTreeGini = buildTree(data, Objective.Gini)
+  println("Tree using Gini: ")
+  printTree(decisionTreeGini)
+
 }
